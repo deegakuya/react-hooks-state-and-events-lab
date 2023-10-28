791e9625bcfe145de750aa7fbd41b0979c786b2b
Diana Gakuya <deegakuya@gmail.com>
2023-10-29 00:59:07 +0300

    assignment  1

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  // Define the styles based on whether the item is in the cart or not
+ 
+  const buttonStyle = inCart ? { backgroundColor: 'rgb(253, 4, 220)' } : { backgroundColor: 'rgb(237, 237, 32)' };
+
+  const toggleInCart = () => {
+    setInCart(prevState => !prevState);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""} >
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+        className={`add ${inCart ? "in-cart-btn" : ""}`} 
+        onClick={toggleInCart}
+        style={buttonStyle}
+      >
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
